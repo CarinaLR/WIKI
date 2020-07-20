155a1453b0151feaea30ec5093e982cc920b2cac
CarinaLR <carinalopez@Carinas-MacBook-Pro.local>
2020-07-20 18:55:43 -0400

    add title

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
 from . import util
 
@@ -6,12 +7,14 @@ from . import util
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+
     })
 
 
 def entry_page(request, title):
     if request.method == "GET":
         return render(request, "encyclopedia/entry_page.html", {
+            "header": title.capitalize(),
             "title": util.get_entry(title)
         })
     else:
